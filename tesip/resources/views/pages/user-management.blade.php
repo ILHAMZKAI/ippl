5e0ca999f174d84471f6ee83dcc14865c7887415
ILHAMZKAI <89698221+ILHAMZKAI@users.noreply.github.com>
2023-09-11 13:43:05 +0800

    s

---
+++ tesip/resources/views/pages/user-management.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Manajemen Akun'])
+@endsection
---
+++ tesip/resources/views/pages/user-profile.blade.php
@@ -0,0 +1,115 @@
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Profil Anda'])
+    <div class="card shadow-lg mx-4 mt-3 card-profile-bottom">
+        <div class="card-body p-3">
+            <div class="row gx-4">
+                <div class="col-auto">
+                    <div class="avatar avatar-xl position-relative">
+                        <img src="/img/team-1.jpg" alt="profile_image" class="w-100 border-radius-lg shadow-sm">
+                    </div>
+                </div>
+                <div class="col-auto my-auto mt-n4">
+                    <div class="h-100">
+                        <h5 class="mb-1">
+                            {{ auth()->user()->firstname ?? 'Nama depan' }} {{ auth()->user()->lastname ?? 'Nama belakang' }}
+                        </h5>
+                        <p class="mb-0 font-weight-bold text-sm">
+                            Gelar
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="alert">
+        @include('components.alert')
+    </div>
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="card">
+                    <form role="form" method="POST" action={{ route('profile.update') }} enctype="multipart/form-data">
+                        @csrf
+                        <div class="card-header-0 pb-0">
+                            <div class="d-flex align-items-center">
+                                <p class="mb-0 mx-4">Ubah Profil</p>
+                                <button type="submit" class="btn btn-primary btn-sm mt-3 mx-auto">Simpan</button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <p class="text-uppercase text-sm">Informasi Pengguna</p>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Nama panggilan</label>
+                                        <input class="form-control" type="text" name="username" value="{{ old('username', auth()->user()->username) }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Alamat Email</label>
+                                        <input class="form-control" type="email" name="email" value="{{ old('email', auth()->user()->email) }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Nama depan</label>
+                                        <input class="form-control" type="text" name="firstname"  value="{{ old('firstname', auth()->user()->firstname) }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Nama belakang</label>
+                                        <input class="form-control" type="text" name="lastname" value="{{ old('lastname', auth()->user()->lastname) }}">
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="horizontal dark">
+                            <p class="text-uppercase text-sm">Informasi Kontak</p>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Alamat</label>
+                                        <input class="form-control" type="text" name="address"
+                                            value="{{ old('address', auth()->user()->address) }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Kota</label>
+                                        <input class="form-control" type="text" name="city" value="{{ old('city', auth()->user()->city) }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Negara</label>
+                                        <input class="form-control" type="text" name="country" value="{{ old('country', auth()->user()->country) }}">
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Kode pos</label>
+                                        <input class="form-control" type="text" name="postal" value="{{ old('postal', auth()->user()->postal) }}">
+                                    </div>
+                                </div>
+                            </div>
+                            <hr class="horizontal dark">
+                            <p class="text-uppercase text-sm">Tentang</p>
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group">
+                                        <label for="example-text-input" class="form-control-label">Tentang saya</label>
+                                        <input class="form-control" type="text" name="about"
+                                            value="{{ old('about', auth()->user()->about) }}">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
