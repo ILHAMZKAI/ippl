8fbf3fb13906a15b496ebd056868df7bca4e778f
ILHAMZKAI <89698221+ILHAMZKAI@users.noreply.github.com>
2023-09-17 18:58:43 +0800

    s

--- tesip/resources/views/layouts/app.blade.php
+++ tesip/resources/views/layouts/app.blade.php
@@ -32,10 +32,10 @@
             @yield('content')
         @else
             @if (!in_array(request()->route()->getName(), ['profile', 'profile-static']))
-                <div class="min-height-300 bg-primary position-absolute w-100"></div>
+                <div class="height-300 bg-primary position-absolute w-100" style="background-image: url('https://github.com/ILHAMZKAI/ippl/blob/main/background/bg100.jpg?raw=true'); background-position-y: 50%;"></div>
             @elseif (in_array(request()->route()->getName(), ['profile-static', 'profile']))
-                <div class="position-absolute w-100 min-height-300 top-0" style="background-image: url('https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/profile-layout-header.jpg'); background-position-y: 50%;">
-                    <span class="mask bg-primary opacity-6"></span>
+                <div class="position-absolute w-100 min-height-400 top-0" style="background-image: url('https://github.com/ILHAMZKAI/ippl/blob/main/background/bg100.jpg?raw=true'); background-position-y: 50%;">
+                    <span class="mask bg-primary opacity-5"></span>
                 </div>
             @endif
             @include('layouts.navbars.auth.sidenav')
