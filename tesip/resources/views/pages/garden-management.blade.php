a16eb17626265135e70246071079152881a17509
ILHAMZKAI <89698221+ILHAMZKAI@users.noreply.github.com>
2023-09-25 00:35:20 +0800

    A

--- tesip/resources/views/pages/garden-management.blade.php
+++ tesip/resources/views/pages/garden-management.blade.php
@@ -11,6 +11,83 @@
                 <div class="card-body px-0 pt-0 pb-2">
                     <div class="table-responsive p-0">
                         <table class="table align-items-center mb-0">
+                            <!DOCTYPE html>
+                            <html>
+                                <head>
+                                    <style>
+                                    /* Gaya untuk sel catur */
+                                    .chessboard {
+                                        display: grid;
+                                        grid-template-columns: repeat(8, 50px); /* 8 kolom */
+                                        grid-template-rows: repeat(8, 50px); /* 8 baris */
+                                    }
+                                    .dark {
+                                        background-color: #769656; /* Warna sel gelap */
+                                    }.light {
+                                        background-color: #0000; /* Warna sel terang */
+                                    }
+                                    </style>
+                                    </head>
+                                    <body>
+                                        <div class="chessboard">
+                                             <!-- Baris 1 -->
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <!-- Baris 2 -->
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <!-- Baris 3 (dan seterusnya) -->
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <div class="light"></div>
+                                             <div class="dark"></div>
+                                             <!-- Silakan tambahkan sel-sel sisa sesuai dengan pola catur yang diinginkan -->
+                                            </div>
+                                        </body>
+                                        </html>
                         </table>
                     </div>
                 </div>
