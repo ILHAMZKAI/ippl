791cfffefe5d3f936868ca953a7ffc50c3aad5bb
ILHAMZKAI <89698221+ILHAMZKAI@users.noreply.github.com>
2023-10-01 16:20:17 +0800

    add

--- tesip/resources/views/layouts/navbars/auth/sidenav.blade.php
+++ tesip/resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -1,10 +1,7 @@
-<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 "
-    id="sidenav-main">
+<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
     <div class="sidenav-header">
-        <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-            aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="{{ route('home') }}"
-            target="_blank">
+        <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+        <a class="navbar-brand m-0" href="{{ route('home') }}" target="_blank">
             <img src="./img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-2 font-weight-bold">GoEstate</span>
         </a>
@@ -14,8 +11,7 @@
         <ul class="navbar-nav">
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() == 'home' ? 'active' : '' }}" href="{{ route('home') }}">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-tv-2 text-dark text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1">Dasbor</span>
@@ -26,8 +22,7 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() == 'profile' ? 'active' : '' }}" href="{{ route('profile') }}">
-                    <div
-                        class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                    <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
                     </div>
                     <span class="nav-link-text ms-1">Manajemen Pengguna</span>
@@ -45,16 +40,14 @@
     </div>
     <div class="sidenav-footer">
         <div class="card card-plain shadow-none" id="sidenavCard">
-            <img class="w-50 mx-auto" src="/img/illustrations/icon-documentation-warning.svg"
-                alt="sidebar_illustration">
-            <div class="card-body text-center p-3 w-100 pt-0">
+            <img class="w-50 mx-auto" src="/img/illustrations/icon-documentation-warning.svg" alt="sidebar_illustration">
+            <div class="card-body text-center p-3 w-100 pt-0 mt-n4">
                 <div class="docs-info">
-                    <h6 class="mb-0">Butuh bantuan?</h6>
-                    <p class="text-xs font-weight-bold mb-0">Klik tombol dibawah</p>
+                    <h6 class="mb-0">Butuh bantuan? Atau saran dari Anda untuk kami</h6>
+                    <p class="text-xs font-weight-bold mb-n1 mt-1">Klik tombol dibawah</p>
                 </div>
             </div>
         </div>
-        <a href="https://wa.me/085705557672" target="https://wa.me/085705557672"
-            class="btn btn-dark btn-sm w-102 mb-0 mx-13">Hubungi kami</a>
-        </div>
+        <a href="https://wa.me/085705557672" target="https://wa.me/085705557672" class="btn btn-dark btn-sm w-102 mb-0 mx-13">Hubungi kami</a>
+    </div>
 </aside>
