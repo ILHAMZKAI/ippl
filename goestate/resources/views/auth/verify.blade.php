975e8e6fa03667622b55ae39f95c39413701de40
FelixDeanJ <108813645+FelixDeanJ@users.noreply.github.com>
2023-10-08 19:12:09 +0800

    new

--- goestate/resources/views/auth/verify.blade.php
+++ goestate/resources/views/auth/verify.blade.php
@@ -1,29 +1,19 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<h1>CLICK BELOW TO VERIFY EMAIL</h1>
 
-                    <div class="card-body">
-                        @if (session('resent'))
-                            <div class="alert alert-success" role="alert">
-                                {{ __('A fresh verification link has been sent to your email address.') }}
-                            </div>
-                        @endif
+<body>
+    <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
+        @csrf
+        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to verify') }}</button>
+    </form>
+</body>
 
-                        {{ __('Before proceeding, please check your email for a verification link.') }}
-                        {{ __('If you did not receive the email') }},
-                        <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
-                            @csrf
-                            <button type="submit"
-                                class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-@endsection
+</html>
--- goestate/routes/web.php
+++ goestate/routes/web.php
@@ -32,7 +32,6 @@ Route::get('/change-password', [ChangePassword::class, 'show'])->middleware('gue
 Route::post('/change-password', [ChangePassword::class, 'update'])->middleware('guest')->name('change.perform');
 Route::get('/dashboard', [HomeController::class, 'index'])->name('home')->middleware('auth', 'verified');
 Route::post('/user-profile/delete-image', [UserProfileController::class, 'deleteImage'])->name('user-profile.delete-image');
-// Route::get('send_email',[RegisterController::class, 'email'])->middleware('guest')->name('register.perform');
 Route::group(['middleware' => 'auth'], function () {
 	Route::get('/virtual-reality', [PageController::class, 'vr'])->name('virtual-reality');
 	Route::get('/rtl', [PageController::class, 'rtl'])->name('rtl');
