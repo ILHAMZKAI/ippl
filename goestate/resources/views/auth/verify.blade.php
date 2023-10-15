39aaf6a110a5189f670dd8125302b5b100955022
FelixDeanJ <108813645+FelixDeanJ@users.noreply.github.com>
2023-10-15 13:10:38 +0800

    + tampilan untuk verif email

--- goestate/resources/views/auth/verify.blade.php
+++ goestate/resources/views/auth/verify.blade.php
@@ -1,19 +1,75 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background: url('{{ asset('img/bg102.jpg') }}') no-repeat center top fixed, #f4f4f4;
+            background-size: cover, auto;
+        }
+
+        .container {
+            background-color: rgba(255, 255, 255, 0.8);
+            max-width: 500px;
+            margin: 0 auto;
+            padding: 20px;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+        }
+
+        h1 {
+            color: #333;
+        }
+
+        p {
+            color: #777;
+        }
+
+        .btn-verify {
+            background-color: #12e655;
+            color: #fff;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            transition: background-color 0.3s;
+        }
+
+        .btn-verify:hover {
+            background-color: #179d08;
+        }
+
+        /* Styling for the "Request Another" link */
+        .btn-request {
+            text-decoration: underline;
+            color: #007BFF;
+            cursor: pointer;
+        }
+
+        .btn-request:hover {
+            color: #0056b3;
+        }
+    </style>
 </head>
-<h1>CLICK BELOW TO VERIFY EMAIL</h1>
 
 <body>
-    <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
-        @csrf
-        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to verify') }}</button>
-    </form>
+    <div class="container">
+        <h1>Verifikasi Email</h1>
+        <p>Silakan klik tombol di bawah untuk verifikasi email Anda.</p>
+        <form class="d-inline" method="POST" action="{{ route('verification.send') }}">
+            @csrf
+            <button type="submit" class="btn btn-verify"
+                onclick="showVerificationAlert()">{{ __('Klik untuk verifikasi') }}</button>
+        </form>
+    </div>
+
+    <script>
+        function showVerificationAlert() {
+            alert("Verifikasi email telah dikirim. Silakan cek email Anda.");
+        }
+    </script>
 </body>
 
 </html>
